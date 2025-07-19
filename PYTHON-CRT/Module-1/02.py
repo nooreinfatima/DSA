20915013978b6c59459f3227a348e10654a39ff3
nooreinfatima <nooreinfatima43@gmail.com>
2025-07-19 17:58:19 +0000

    adding newfiles

---
+++ PYTHON-CRT/Module-1/02.py
@@ -0,0 +1,8 @@
+num=23
+word='IT'
+decimal_num=23.33
+logic=True
+print("enter the num type ",type(num))
+print("enter the word type ",type(word))
+print("enter the decimal_num type",type(decimal_num))
+print("enter the logic type",type(logic))
